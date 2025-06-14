cb3cb1398a6ce5ddaaf0e8185da9cb47bfdd7fff
Hemanth-1354 <hemanthkumar_polisetti@srmap.edu.in>
2025-06-14 16:47:28 +0530

    Minor changes in UI

--- HireCircle/settings.py
+++ HireCircle/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from decouple import config
 import os
 from pathlib import Path
-import 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
